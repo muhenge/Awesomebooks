db2d2813be4f5349df23529491924ec52fbbe70c
muhenge <mugunga5@gmail.com>
2021-07-05 23:53:28 +0200

    Add book - author to the page

--- index.js
+++ index.js
@@ -4,17 +4,42 @@ let books = [];
 const form = document.getElementsByTagName('form');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
+const ul = document.getElementById('ul');
+;
+const addBook = ()=>{
+    // const makeBook = ()=>{
+        const book = {
+            name: title.value,
+            author: author.value
+        };
+        books.push(book);
+        
+        books.forEach(b => {
+            const titleLi = document.createElement('li');
+            const authorLi = document.createElement('li')
+            titleLi.innerText = b.name
+            authorLi.innerText = b.author
+            ul.appendChild(titleLi);
+            ul.appendChild(authorLi);
+        });
 
-const addBook = (event)=>{
-    event.preventDefault();
-    const book = {
-        name: title.value,
-        author: author.value
-    };
-    books.push(book);
+    // const display = ()=>{
+        
+    // }
+
+    // return {makeBook,display}
 }
 
-document.getElementById('btn').addEventListener('click', (addBook));
+// const test = addBook();
+
+const removeBook = () => {
+
+}
+
+document.getElementById('btn').addEventListener('click', (event)=>{
+    event.preventDefault();
+    addBook();
+});
 
 
 
