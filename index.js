e4ffe580ede876cb322fc1ba6b02ab44dfe9b206
Maximiliano Diaz <maxi_diaz@mail.com>
2021-07-12 18:21:39 -0300

    Move code into class body.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let arrayOfBooks = [];
+/*let arrayOfBooks = [];
 
 const addButton = document.getElementById("add");
 const bookList = document.getElementById("book-list");
@@ -36,7 +36,7 @@ function loadBooks() {
 
   if (strBooks !== null) {
     arrayOfBooks = JSON.parse(strBooks);
-    /* eslint-disable */
+    
 
     for (const book of arrayOfBooks) {
       const bookCont = document.createElement("div");
@@ -56,13 +56,13 @@ function loadBooks() {
       bookList.appendChild(bookCont);
     }
   }
-  /* eslint-enable */
+
 }
 
 function saveBooks() {
   const svBooks = JSON.stringify(arrayOfBooks);
   window.localStorage.setItem('books', svBooks);
-}
+}*/
 
 // window.addEventListener('load', loadBooks);
 // window.addEventListener('unload', saveBooks);
@@ -78,23 +78,39 @@ class BookCatalog {
 
   addBook() {
     const newBook = {
-      title: titleInput.value,
-      author: authorInput.value,
+      title: this.inputTitle.value,
+      author: this.inputAuthor.value,
     };
-  
+    console.log(newBook);
+    console.log(this.booksCatalog);
     if (newBook.title !== "" && newBook.author !== "") {
-      arrayOfBooks.push(newBook);
+      this.booksCatalog.push(newBook);
     }
   }
-  removeBook() {}
+
+  removeBook(event) {
+    const bookKey = event.currentTarget.parentElement;
+    const bookContainer = bookKey.parentElement;
+
+    const filteredArray = this.booksCatalog.filter(
+    (book) =>
+      book.title !== bookKey.children[0].innerText ||
+      book.author !== bookKey.children[1].innerText
+    );
+
+    arrayOfBooks = filteredArray;
+
+    bookContainer.removeChild(bookKey);
+  }
+
   loadBooks() {
     const strBooks = window.localStorage.getItem("books");
 
     if (strBooks !== null) {
-      arrayOfBooks = JSON.parse(strBooks);
+      this.booksCatalog = JSON.parse(strBooks);
       /* eslint-disable */
   
-      for (const book of arrayOfBooks) {
+      for (const book of this.booksCatalog) {
         const bookCont = document.createElement("div");
         const bookName = document.createElement("p");
         const authorName = document.createElement("p");
@@ -115,15 +131,35 @@ class BookCatalog {
     /* eslint-enable */
   }
   saveBooks() {
-    const svBooks = JSON.stringify(arrayOfBooks);
-  window.localStorage.setItem('books', svBooks);
+    const svBooks = JSON.stringify(this.booksCatalog);
+    window.localStorage.setItem('books', svBooks);
   }
 
-  initialize() {}
+  initialize() {
+    //this.loadBooks();
+    this.addBookButton = document.getElementById("add");
+    this.listOfBooks = document.getElementById("book-list");
+    this.inputTitle = document.getElementById("title-name");
+    this.inputAuthor = document.getElementById("author-name");
+    this.listOfBooks = document.getElementById("book-list");
+    this.inputTitle = document.getElementById("title-name");
+    this.inputAuthor = document.getElementById("author-name");
+    document.getElementById('add').addEventListener('click', this.addBook);
+    window.addEventListener('load', this.loadBooks);
+    window.addEventListener('unload', this.saveBooks);
+  }
 };
 
 const b = new BookCatalog();
 
-document.getElementById('add').addEventListener('click', b.addBook());
+b.initialize();
+/*document.getElementById('add').addEventListener('click', b.addBook());
 window.addEventListener('load', b.loadBooks());
-window.addEventListener('unload', b.saveBooks());
+window.addEventListener('unload', b.saveBooks());/*
+
+/*
+  initialize (){
+    loadBooks();
+    addEventListener(*);
+  }
+*/
