968ef4ff0633d64da58dfaeaa1d06656af992a70
Maximiliano Diaz <maxi_diaz@mail.com>
2021-07-16 17:53:47 -0300

    Correct booklist not updating immediately.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { DateTime } = luxon;
+
 const documentVariables = {
   booksCatalog: [],
   listOfBooks: document.getElementById('book-list'),
@@ -9,8 +11,8 @@ const documentVariables = {
 class BookCatalog {
   constructor() {
     documentVariables.addBookButton.addEventListener('click', this.addBook);
-    window.addEventListener('load', this.loadBooks);
-    window.addEventListener('unload', this.saveBooks);
+    window.addEventListener('load', BookCatalog.loadBooks);
+    window.addEventListener('unload', BookCatalog.saveBooks);
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -23,30 +25,42 @@ class BookCatalog {
       documentVariables.booksCatalog.push(newBook);
     }
     document.getElementById('input-form').reset();
+
+    BookCatalog.saveBooks();
+    //console.log(documentVariables.booksCatalog);
+    BookCatalog.loadBooks();
+    //console.log(documentVariables.booksCatalog);
   }
 
   // eslint-disable-next-line class-methods-use-this
-  loadBooks() {
+static  loadBooks() {
     const strBooks = window.localStorage.getItem('books');
     if (strBooks !== null) {
+      documentVariables.listOfBooks.innerHTML = '';
       documentVariables.booksCatalog = JSON.parse(strBooks);
       /* eslint-disable */
 
       for (const book of documentVariables.booksCatalog) {
         const bookCont = document.createElement('div');
+        
         const titleAuth = document.createElement('div');
         const titleAuthClasses = 'd-flex flex-column justify-content-around'.split(' ');
         titleAuth.classList.add(...titleAuthClasses);
+        
         const bookName = document.createElement('span');
         bookName.classList.add('show-no-bootstrap', 'text-uppercase', 'fs-2');
+        
         const authorName = document.createElement('span');
         authorName.classList.add('show-no-bootstrap');
-        titleAuth.appendChild(bookName);
-        titleAuth.appendChild(authorName);
-        const bookContClasses = 'col-12 d-flex justify-content-between border p-3'.split(' ');
-        bookCont.classList.add(...bookContClasses);
+        
         bookName.innerText = book.title;
         authorName.innerText = book.author;
+
+        titleAuth.appendChild(bookName);
+        titleAuth.appendChild(authorName);
+        
+        const bookContClasses = 'col-12 d-flex justify-content-between border p-3'.split(' ');
+        bookCont.classList.add(...bookContClasses);
 
         const rmvBookBtn = document.createElement('button');
         rmvBookBtn.innerText = 'Remove Book';
@@ -60,6 +74,9 @@ class BookCatalog {
 
     }
     /* eslint-enable */
+    console.log('loadBooks()')
+    console.log(window.localStorage.getItem('books'));
+    console.log(documentVariables.booksCatalog);
   }
 
   static removeBook(event) {
@@ -76,16 +93,22 @@ class BookCatalog {
   }
 
   // eslint-disable-next-line class-methods-use-this
-  saveBooks() {
-    if (documentVariables.booksCatalog !== undefined) {
+static  saveBooks() {
       const svBooks = JSON.stringify(documentVariables.booksCatalog);
       window.localStorage.setItem('books', svBooks);
+      console.log('saveBooks()');
     }
-  }
 }
 
 // eslint-disable-next-line no-unused-vars
 const b = new BookCatalog();
+
+const timeP = document.createElement('p');
+timeP.classList.add('align-self-end');
+
+timeP.textContent = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+
+document.getElementById('date-time').appendChild(timeP);
 
 document.getElementById('input-form').classList.add('hide-no-bootstrap');
 document.getElementById('contact').classList.add('hide-no-bootstrap');
